44929d7533c5f411b2c0e029d92d354d655daaf9
Alex Pasieka <alex.pasieka00@gmail.com>
2019-03-13 21:36:24 -0400

    chap03 20190313a

---
+++ chap04-insert-gardening-tools.js
@@ -0,0 +1,19 @@
+db.categories.insert({
+    _id: ObjectId("6a5b1476238d3b4dd5000048"),
+    slug: "gardening-tools",
+    name: "Gardening Tools",
+    description: "Gardening gadgets galore!",
+    parent_id: ObjectId("55804822812cb336b78728f9"),
+        ancestors: [
+          {
+            name: "Home",
+            _id: ObjectId("558048f0812cb336b78728fa"),
+            slug: "home"
+          },
+          {
+              name: "Outdoors",
+              _id: ObjectId("55804822812cb336b78728f9"),
+              slug: "outdors"
+          }
+        ]
+      })
