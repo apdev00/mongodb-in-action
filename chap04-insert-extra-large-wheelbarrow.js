ec9c4e6762722792ef112ecf7abcdf4fe231dfbf
Alex Pasieka <alex.pasieka00@gmail.com>
2019-03-20 08:22:31 -0400

    chap04 20190320a

---
+++ chap04-insert-extra-large-wheelbarrow.js
@@ -0,0 +1,40 @@
+db.products.insert(
+    {
+        _id: ObjectId("4c4b1476238d3b4dd5003981"),
+        slug: "wheelbarrow-9092",
+        sku: "9092",
+        name: "Extra Large Wheelbarrow",
+        description: "Heavy duty wheelbarrow...",
+        details: {
+            weight: 47,
+            weight_units: "lbs",
+            model_num: 4039283402,
+            manufacturer: "Acme",
+            color: "Green"
+        },
+        total_reviews: 4,
+        average_review: 4.5,
+        pricing: {
+            retail: 589700,
+            sale: 489700
+        },
+        price_history: [{
+            retail: 529700,
+            sale: 429700,
+            start: new Date(2018, 4, 1),
+            end: new Date(2018, 4, 8)
+        },
+        {
+            retail: 529700,
+            sale: 529700,
+            start: new Date(2018, 4, 9),
+            end: new Date(2018, 4, 16)
+        }],
+        primary_category: ObjectId("6a5b1476238d3b4dd5000048"),
+        category_ids: [
+            ObjectId("6a5b1476238d3b4dd5000048"),
+            ObjectId("6a5b1476238d3b4dd5000048")
+        ],
+        main_cat_id: ObjectId("6a5b1476238d3b4dd5000048"),
+        tags: ["tools", "gardening", "soil"]
+    })
---
+++ chap04-insert-order-page81.js
@@ -0,0 +1,34 @@
+db.orders.insert({
+    _id: ObjectId("6a5b1476238d3b4dd5000048"),
+    user_id: ObjectId("4c4b1476238d3b4dd5000001"),
+    state: "CART",
+    line_items: [
+    {
+        _id: ObjectId("4c4b1476238d3b4dd5003981"),
+        sku: "9092",
+        name: "Extra Large Wheelbarrow",
+        quantity: 1,
+        pricing: {
+            retail: 5897,
+            sale: 4897
+        }
+    },
+    {
+        _id: ObjectId("4c4b1476238d3b4dd5003982"),
+        sku: "10027",
+        name: "Rubberized Work Gloves, Black",
+        quantity: 2,
+        pricing: {
+            retail: 1499,
+            sale: 1299
+        }
+    }
+    ],
+    shipping_address: {
+        street: "588 5th Street",
+        city: "Brooklyn",
+        state: "NY",
+        zip: 11215
+    },
+    sub_total: 6196
+})
